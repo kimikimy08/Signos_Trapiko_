9fbf0329b735875ee86bce438679932f4a7806de
kimikimy08 <104545752+kimikimy08@users.noreply.github.com>
2022-11-24 05:21:06 +0800

    Initial Commit

--- signosTrapiko/wsgi.py
+++ signosTrapiko/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signos_link.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signosTrapiko.settings')
 
 application = get_wsgi_application()
