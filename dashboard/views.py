b1c512ae83a121f7e5d2a176ae379db030a2f43f
kimikimy08 <104545752+kimikimy08@users.noreply.github.com>
2022-12-20 16:16:09 +0800

    fix dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -333,7 +333,82 @@ def superadmin_dashboard(request):
     # df = df.dropna(axis=0, subset=['user_report__latitude', 'user_report__longitude', 'accident_factor', 'user_report__date'])
     # mapquestopen
     
+    today = datetime.today()
+    yesterday = (today - timedelta(days=1)).strftime("%Y-%m-%d")
+    threedays = (today - timedelta(days=3)).strftime("%Y-%m-%d")
+    one_week_ago = (today - timedelta(days=7)).strftime("%Y-%m-%d")
+    thirty_days_ago = (today - timedelta(days=360)).strftime("%Y-%m-%d")
+    today = today.strftime("%Y-%m-%d")
+
+    print("Today     :",today ,
+        "\nYesterday :",yesterday ,
+        "\nT-3       :",threedays ,
+        "\nT-7       :",one_week_ago ,
+        "\nT-30      :",thirty_days_ago  )
     
+    #Converting submission_date to datetime 
+    df["date"] = pd.to_datetime(df.date)
+    #Filtering Three days back data 
+    date_df = df[df['date'] > thirty_days_ago]
+    print(date_df)
+    
+    df_location = pd.DataFrame(incident_general.values('latitude', 'longitude'))
+    print(df_location)
+    
+    map1 = folium.Map(location=[14.676208, 121.043861],
+                      zoom_start=12)
+
+    fg = folium.FeatureGroup(name='Marker Cluster', show=False)
+    map1.add_child(fg)
+    
+    # HeatMap(covid_heatmap_df, 
+    #     min_opacity=0.4,
+    #     blur = 18
+    #            ).add_to(folium.FeatureGroup(name='Heat Map').add_to(hm))
+
+    fg2 = folium.FeatureGroup(name='Heat Map', show=True)
+    map1.add_child(fg2)
+
+    plugins.HeatMap(df_location).add_to(fg2)
+    FastMarkerCluster(data=df_location.values.tolist()).add_to(fg)
+    # marker_cluster = MarkerCluster().add_to(fg)
+    folium.TileLayer(('openstreetmap'), attr='openstreetmap').add_to(map1)
+    # folium.TileLayer('mapquestopen', attr='mapquestopen').add_to(map1)
+    # folium.TileLayer('MapQuest Open Aerial', attr='MapQuest Open Aerial').add_to(map1)
+    folium.TileLayer('cartodbpositron', attr='cartodbpositron').add_to(map1)
+    folium.TileLayer('cartodbdark_matter',
+                     attr='cartodbdark_matter').add_to(map1)
+    plugins.Fullscreen(position='topright').add_to(map1)
+    folium.LayerControl().add_to(map1)
+
+    plugins.HeatMap(df_location).add_to(fg2)
+    
+    currentmonth_df = date_df[(date_df['date'] > thirty_days_ago)]
+    print(currentmonth_df)
+    
+    inc_heatmaptime_df = currentmonth_df.merge(df_location, how='left')[['date','latitude','longitude']]
+    inc_heatmaptime_df.dropna(inplace=True)
+    print(inc_heatmaptime_df)
+    
+    time_index = list(inc_heatmaptime_df['date'].sort_values().astype('str').unique())
+    print(time_index)
+    
+    inc_heatmaptime_df['date'] = inc_heatmaptime_df['date'].sort_values(ascending=True)
+    data = []
+    for _, d in inc_heatmaptime_df.groupby('date'):
+        data.append([[row['latitude'], row['longitude']] for _, row in d.iterrows()])
+    print(data)
+    
+    hmt = folium.Map(location=[14.676208, 121.043861],
+               tiles='cartodbpositron',#'cartodbpositron', stamentoner
+               zoom_start=12,
+               control_scale=True)
+
+    HeatMapWithTime(data,
+                    index=time_index,
+                    auto_play=True,
+                    use_local_extrema=True
+                ).add_to(hmt)
     
     # fg2 = folium.FeatureGroup(name='Heat Map with Time', show=False)
     # map.add_child(fg2)
@@ -356,13 +431,13 @@ def superadmin_dashboard(request):
         folium.CircleMarker(location=[row['latitude'],
                             row['longitude']], radius=2, fill=True).add_to(map2)
 
-   
+    map1 = map1._repr_html_()
     map2 = map2._repr_html_()
-
+    hmt = hmt._repr_html_()
     context = {
-      
+        'map1': map1,
         'map2': map2,
-   
+        'hmt': hmt,
         'labels': labels,
         'data': data,
         'labels1': labels1,
